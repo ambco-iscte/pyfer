d4578af3fe3410b7d6d5acdca5e04f4df30caa2f
WildPiggie <wildpiggie@gmail.com>
2023-11-23 13:35:19 +0000

    Minor changes to classifier.py and comments

--- model/recogniser/classifier.py
+++ model/recogniser/classifier.py
@@ -31,10 +31,10 @@ def fit(
     :param model: The Torch NN model to be fitted.
     :param loss_function: Loss function to use.
     :param optimizer: Gradient optimizer to use.
-    :param x_train: TODO
-    :param x_val: TODO
-    :param y_train: TODO
-    :param y_val: TODO
+    :param x_train: List of features for training.
+    :param x_val: List of features for validation.
+    :param y_train: List of labels of training data.
+    :param y_val: List of labels of validation data.
     :param epochs: Number of epochs to run.
     :param batch_size: TODO
     :return: A dictionary containing the history of training and validation set metrics along the training process.
@@ -49,15 +49,28 @@ def fit(
         metrics[f'{name}_recall'].append(recall_score(predicted, y_true))
         metrics[f'{name}_f1'].append(f1_score(predicted, y_true))
 
+    '''
+    # Fazer algo deste genero pode ser importante
+    if phase == 'train':
+        model.train()  # Set model to training mode
+    else:
+        model.eval()   # Set model to evaluate mode
+    '''
+
     for epoch in range(epochs):
         for i in range(0, len(x_train), batch_size):
-            x_batch = x_train[i:i + batch_size]
-            y_pred = model(x_batch)
-            y_batch = y_train[i:i + batch_size]
-            loss = loss_function(y_pred, y_batch)
             optimizer.zero_grad()
+
+            x_batch = x_train[i:i + batch_size]
+            y_batch = y_train[i:i + batch_size]
+
+            y_pred = model(x_batch)
+            loss = loss_function(y_pred, y_batch)
+
             loss.backward()
             optimizer.step()
+
+            print(f'Loss: {loss.item()}')
 
     append('train', x_train, y_train)
     append('val', x_val, y_val)
@@ -69,8 +82,21 @@ def main():
     # Build model
     # TODO:
     #  How do we set trainable=False?
-    #  How do we set include_top?
-    pretrained_model = mobilenet_v3_small(pretrained=True)
+
+    input_size = 2304  # 48*48
+    num_classes = 7
+
+    pretrained_model = mobilenet_v3_small(weights="MobileNet_V3_Small_Weights.DEFAULT")
+    # Equivalent to setting include_top = False
+    for param in pretrained_model.parameters():
+        param.requires_grad = False
+
+    '''
+    # Unfreeze last layer
+    for param in resnet18.fc.parameters():
+        param.requires_grad = True
+    '''
+
     model = nn.Sequential(
         pretrained_model,  # TODO: Remember to rescale images by 1./255
 
@@ -78,16 +104,18 @@ def main():
         nn.Flatten(),
 
         # Dense layer with ReLU activation
-        nn.Linear(),  # TODO: figure out in_features and out_features
+        nn.Linear(input_size, input_size),  # TODO: figure out in_features and out_features
         nn.ReLU(),
 
         # Dropout to avoid over-fitting
         nn.Dropout(0.2),
 
         # Dense layer with Softmax activation
-        nn.Linear(),  # TODO: figure out in_features and out_features (is out_features = num_classes here?)
+        nn.Linear(input_size, num_classes),
+        # TODO: figure out in_features and out_features (is out_features = num_classes here?)
         nn.Softmax()
     )
+
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
@@ -99,6 +127,9 @@ def main():
         0.1
     )
 
+    # TODO Format the labels in a way that the model accepts
+    # Currently, they are a string and the model does not know how to interpret this
+
     # Fit model to training data
     epochs = 100
     batch_size = 10
@@ -109,7 +140,7 @@ def main():
         y_pred = model(x_test)
         report = classification_report(y_test, y_pred)
         # TODO: analyse report
-        
+
     # TODO: save model to file if wanted
 
 
